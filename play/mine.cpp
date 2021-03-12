4d4101fd4367a228880c7de0ff0f128588bf6265
윤서준 <yys39240814@gmail.com>
2021-03-12 22:53:41 +0900

    zjalt

--- play/mine.cpp
+++ play/mine.cpp
@@ -11,9 +11,10 @@ private:
 	int mine[10005][10005] = {};
 	int cou[10005][10005] = {};
 	int open[10005][10005] = {};
-	int N, M, CM;
+	int check[10005][10005] = {};
+	int N, M, cmine;
 	int input, n;
-	grid input_grid;
+	grid inpg;
 	int X[8] = { 1,0,-1,-1,-1,0,1,1 };
 	int Y[8] = { 1,1,1,0,-1,-1,-1,0 };
 	int fmine(int y, int x) {
@@ -24,6 +25,12 @@ private:
 				f++;
 		}
 		return f;
+	}
+	int count_check(int y, int x) {
+		for (int i = 0; i < 8; i++)
+		{
+
+		}
 	}
 	void print() {
 		for (int i = 1; i <= N; i++)
@@ -37,7 +44,10 @@ private:
 		}
 	}
 	void printp(int y, int x) {
-		if (open[y][x] == 0)
+		if (check[y][x] == 1) {
+			cout << "¤·";
+		}
+		else if (open[y][x] == 0)
 			cout << "¤±";
 		else {
 			if (cou[y][x])
@@ -56,10 +66,21 @@ private:
 			return;
 		for (int i = 0; i < 8; i++)
 		{
-			if (mine[y+Y[i]][x+X[i]] == 0) {
+			if (mine[y + Y[i]][x + X[i]] == 0) {
 				op(y + Y[i], x + X[i]);
 			}
 		}
+	}
+	void init() {
+		memset(mine, 0, sizeof(mine));
+		memset(open, 0, sizeof(open));
+		memset(check, 0, sizeof(check));
+		srand((unsigned int)time(NULL));
+	}
+	void wrong_input() {
+		system("cls");
+		print();
+		cout << "incollect selection\n";
 	}
 	grid ch(int input) {
 		grid tmp;
@@ -69,42 +90,75 @@ private:
 	}
 public:
 	void start() {
-		cin >> N >> M >> CM;
-		srand((unsigned int)time(NULL));
-		int x, y;
-		for (int i = 0; i < CM; i++)
-		{
-			x = rand() % M;
-			y = rand() % N;
-			x++;
-			y++;
-			if (mine[y][x] == 1) {
-				i--;
-				continue;
-			}
-			mine[y][x] = 1;
-		}
-		for (int i = 1; i <= N; i++)
-		{
-			for (int j = 1; j <= M; j++)
-			{
-				cou[i][j] = fmine(i, j);
-			}
-		}
-		system("cls");
-		print();
 		while (1) {
-			cin >> n >> input;
-			input_grid = ch(input);
-			if (open[iny][inx]||iny<=0||iny>N||inx<=0||inx>M) {
-				system("cls");
-				print();
-				cout << "incollect selection\n";
-				continue;
+			cin >> N >> M >> cmine;
+			init();
+			int x, y;
+			for (int i = 0; i < cmine; i++)
+			{
+				x = rand() % M;
+				y = rand() % N;
+				x++;
+				y++;
+				if (mine[y][x] == 1) {
+					i--;
+					continue;
+				}
+				mine[y][x] = 1;
 			}
-			op(iny, inx);
+			for (int i = 1; i <= N; i++)
+			{
+				for (int j = 1; j <= M; j++)
+				{
+					cou[i][j] = fmine(i, j);
+				}
+			}
 			system("cls");
 			print();
+			while (1) {
+				cin >> n >> input;
+				inpg = ch(input);
+				if (inpg.y <= 0 || inpg.y > N || inpg.x <= 0 || inpg.x > M) {
+					wrong_input();
+					continue;
+				}
+				if (n == 0) {
+					if (open[inpg.y][inpg.x] == 1) {
+						wrong_input();
+						continue;
+					}
+					if (mine[inpg.y][inpg.x] == 0) {
+						op(inpg.y, inpg.x);
+					}
+					else {
+						system("cls");
+						cout << "game over!\n\n\nDo you want to play again? y/n\n";
+						char str;
+						cin >> str;
+						if (str == 'y')
+							break;
+						else
+							return;
+					}
+				}
+				else if (n == 1) {
+					if (check[inpg.y][inpg.x] == 0) {
+						check[inpg.y][inpg.x] = 1;
+						if (mine[inpg.y][inpg.x] == 1)
+							cmine--;
+					}
+					else {
+						check[inpg.y][inpg.x] = 0;
+						if (mine[inpg.y][inpg.x] == 1)
+							cmine++;
+					}
+				}
+				else {
+
+				}
+				system("cls");
+				print();
+			}
 		}
 	}
 };
