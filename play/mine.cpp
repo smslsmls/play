6003ad922e6370b75581ecc154d6625a2127eb27
윤서준 <yys39240814@gmail.com>
2021-03-12 22:21:06 +0900

    zjalt

--- play/mine.cpp
+++ play/mine.cpp
@@ -4,10 +4,16 @@ using namespace std;
 
 class find_mine {
 private:
+	typedef struct {
+		int y;
+		int x;
+	}grid;
 	int mine[10005][10005] = {};
 	int cou[10005][10005] = {};
 	int open[10005][10005] = {};
-	int N, M, CM, inx, iny;
+	int N, M, CM;
+	int input, n;
+	grid input_grid;
 	int X[8] = { 1,0,-1,-1,-1,0,1,1 };
 	int Y[8] = { 1,1,1,0,-1,-1,-1,0 };
 	int fmine(int y, int x) {
@@ -20,12 +26,12 @@ private:
 		return f;
 	}
 	void print() {
-		for (int i = 0; i < N; i++)
+		for (int i = 1; i <= N; i++)
 		{
-			for (int j = 0; j < M; j++)
+			for (int j = 1; j <= M; j++)
 			{
 				printp(i, j);
-				cout << mine[i][j];
+				//cout << mine[i][j];
 			}
 			cout << '\n';
 		}
@@ -35,26 +41,31 @@ private:
 			cout << "¤±";
 		else {
 			if (cou[y][x])
-				cout << cou[y][x];
+				cout << ' ' << cou[y][x];
 			else
-				cout << ' ';
+				cout << "  ";
 		}
 	}
 	void op(int y, int x) {
-		if (y <= 0 && x <= 0 && y > N && x > M)
+		if (y <= 0 || x <= 0 || y > N || x > M)
 			return;
 		if (open[y][x] == 1)
 			return;
 		open[y][x] = 1;
-		cou[y][x] = fmine(y, x);
 		if (cou[y][x])
 			return;
 		for (int i = 0; i < 8; i++)
 		{
-			if (fmine(y + Y[i], x + X[i]) == 0) {
+			if (mine[y+Y[i]][x+X[i]] == 0) {
 				op(y + Y[i], x + X[i]);
 			}
 		}
+	}
+	grid ch(int input) {
+		grid tmp;
+		tmp.y = (input / N) + 1;
+		tmp.x = input % M;
+		return tmp;
 	}
 public:
 	void start() {
@@ -73,10 +84,21 @@ public:
 			}
 			mine[y][x] = 1;
 		}
+		for (int i = 1; i <= N; i++)
+		{
+			for (int j = 1; j <= M; j++)
+			{
+				cou[i][j] = fmine(i, j);
+			}
+		}
+		system("cls");
 		print();
 		while (1) {
-			cin >> iny >> inx;
-			if (open[iny][inx]) {
+			cin >> n >> input;
+			input_grid = ch(input);
+			if (open[iny][inx]||iny<=0||iny>N||inx<=0||inx>M) {
+				system("cls");
+				print();
 				cout << "incollect selection\n";
 				continue;
 			}
