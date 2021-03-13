60332578f31f8316ca6d038924ee74d9687b81df
윤서준 <yys39240814@gmail.com>
2021-03-13 21:59:09 +0900

    zjalt

--- play/mine.cpp
+++ play/mine.cpp
@@ -16,6 +16,7 @@ private:
 	int check[35][70] = {};
 	int X[8] = { 1,0,-1,-1,-1,0,1,1 };
 	int Y[8] = { 1,1,1,0,-1,-1,-1,0 };
+	int rp;
 	//입력변수 선언
 	int N, M, cmine;
 	int input, n;
@@ -35,10 +36,13 @@ private:
 	}
 	//주변 표시 갯수 반환
 	int count_check(int y, int x) {
+		int c = 0;
 		for (int i = 0; i < 8; i++)
 		{
-
+			if (check[y + Y[i]][x + X[i]])
+				c++;
 		}
+		return c;
 	}
 	//맵 출력
 	void print() {
@@ -82,6 +86,39 @@ private:
 			}
 		}
 	}
+	//한 점 열기 확인
+	int opche(int y, int x) {
+		if (mine[y][x] == 0) {
+			op(y, x);
+			rp = -1;
+			return 1;
+		}
+		else {
+			system("cls");
+			cout << "game over!\n\n\nDo you want to play again? y/n\n";
+			char str;
+			while (1) {
+				cin >> str;
+				if (str!='y'&&str!='x') {
+					system("cls");
+					wrong_input();
+					cout << "input y or n\n";
+				}
+				else {
+					break;
+				}
+			}
+			if (str == 'y') {
+				system("cls");
+				rp = 1;
+				return 0;
+			}
+			else {
+				rp = 0;
+				return 0;
+			}
+		}
+	}
 	//기본 배열 초기화, 랜덤 시드 초기화
 	void init() {
 		memset(mine, 0, sizeof(mine));
@@ -95,6 +132,54 @@ private:
 		print();
 		cout << "incollect selection\n";
 	}
+	//기본 인풋
+	void stdinp() {
+		e = 0;
+		cin >> N >> M >> cmine;
+		if (N < 1 || N>29 || M < 1 || M>60 || cmine<1 || cmine>N * M - 1)
+			system("cls");
+		if (N < 1 || N>29) {
+			cout << "select wigth 1~29\n";
+			e++;
+		}
+		if (M < 1 || M>60) {
+			cout << "select length 1~60\n";
+			e++;
+		}
+		if (cmine<1 || cmine>N * M - 1) {
+			cout << "select mine under area\n";
+			e++;
+		}
+		return;
+	}
+	//지뢰 생성
+	void spmine() {
+		int x, y;
+		for (int i = 0; i < cmine; i++)
+		{
+			x = rand() % M;
+			y = rand() % N;
+			x++;
+			y++;
+			if (mine[y][x] == 1) {
+				i--;
+				continue;
+			}
+			mine[y][x] = 1;
+		}
+		return;
+	}
+	//cou초기화
+	void init_cou() {
+		for (int i = 1; i <= N; i++)
+		{
+			for (int j = 1; j <= M; j++)
+			{
+				cou[i][j] = fmine(i, j);
+			}
+		}
+		return;
+	}
 	//int to grid
 	grid ch(int input) {
 		grid tmp;
@@ -105,45 +190,12 @@ private:
 public:
 	void start() {
 		while (1) {
-			e = 0;
-			cin >> N >> M >> cmine;
-			if (N < 1 || N>29 || M < 1 || M>60 || cmine<1 || cmine>N * M - 1)
-				system("cls");
-			if (N < 1 || N>29) {
-				cout << "select wigth 1~29\n";
-				e++;
-			}
-			if (M < 1 || M>60) {
-				cout << "select length 1~60\n";
-				e++;
-			}
-			if (cmine<1 || cmine>N * M - 1) {
-				cout << "select mine under area\n";
-				e++;
-			}
+			stdinp();
 			if (e)
 				continue;
 			init();
-			int x, y;Z
-			for (int i = 0; i < cmine; i++)
-			{
-				x = rand() % M;
-				y = rand() % N;
-				x++;
-				y++;
-				if (mine[y][x] == 1) {
-					i--;
-					continue;
-				}
-				mine[y][x] = 1;
-			}
-			for (int i = 1; i <= N; i++)
-			{
-				for (int j = 1; j <= M; j++)
-				{
-					cou[i][j] = fmine(i, j);
-				}
-			}
+			spmine();
+			init_cou();
 			system("cls");
 			print();
 			while (1) {
@@ -155,23 +207,32 @@ public:
 				}
 				if (n == 0) {//칸 열기
 					if (open[inpg.y][inpg.x] == 1) {
-						wrong_input();
-						continue;
-					}
-					if (mine[inpg.y][inpg.x] == 0) {
-						op(inpg.y, inpg.x);
+						if (cou[inpg.y][inpg.x] == count_check(inpg.y, inpg.x)) {
+							for (int i = 0; i < 8; i++)
+							{
+								if (!opche(inpg.y,inpg.x)) {
+									break;
+								}
+							}
+							if (rp != -1) {
+								if (rp)
+									break;
+								else
+									return;
+							}
+						}
+						else {
+							wrong_input();
+							continue;
+						}
 					}
 					else {
-						system("cls");
-						cout << "game over!\n\n\nDo you want to play again? y/n\n";
-						char str;
-						cin >> str;
-						if (str == 'y') {
-							system("cls");
-							break;
+						if (!opche(inpg.y,inpg.x)) {
+							if (rp)
+								break;
+							else
+								return;
 						}
-						else
-							return;
 					}
 				}
 				else if (n == 1) {//지뢰 표시
@@ -187,7 +248,7 @@ public:
 					}
 				}
 				else {
-					if (open[inpg.y][inpg.x] == 0) {
+					if (open[inpg.y][inpg.x] == 1) {
 						wrong_input();
 						continue;
 					}
