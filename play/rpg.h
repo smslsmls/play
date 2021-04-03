556e9d40f68bdb134676e57d78c0f631066248f5
user <user@19>
2021-04-03 18:11:54 +0900

    커밋

--- play/rpg.h
+++ play/rpg.h
@@ -2,12 +2,46 @@
 #include<conio.h>
 #include<Windows.h>
 using namespace std;
+//스탯
+typedef struct {
+	unsigned short STR = 0;//물공
+	unsigned short INT = 0;//마공
+	unsigned short WIS = 0;//마나
+	unsigned short DEX = 0;//방어력
+	unsigned short AGI = 0;//민첩성
+	unsigned short VIT = 0;//체력
+}stat_struct;
+//스테이터스
+typedef struct {
+	int hp;
+	int mp;
+	int atk;
+	int def;
+	int cri;
+}status;
+//선택지 구조체
+typedef struct {
+	string arr[100];
+	int SIZE;
+}selection_array;
+//소지 구조체
+typedef struct {
+	string arr[1000];
+	int idx;
+}inventory;
+//무기 성능
+typedef struct {
+	string name;
+	enum_rarity rarity;
+	int weapon_skill_code;
+	stat_struct plus_stat;
+}weapon_status;
 //직군
 enum job_classes {
 	//물리
 	//레이피어
 	DUELER = 0,//0
-	//검, 월도, 치도, 글러브
+	//검, 월도, 글러브
 	FIGHTER,//1
 	//단검, 비수
 	ASSASSIN,//2
@@ -88,33 +122,97 @@ enum enum_rarity {
 	CONTRABAND,
 	UNOBTAINABLE
 };
-//스탯
-typedef struct {
-	unsigned short STR = 0;//물공
-	unsigned short INT = 0;//마공
-	unsigned short WIS = 0;//마나
-	unsigned short DEX = 0;//방어력
-	unsigned short AGI = 0;//민첩성
-	unsigned short VIT = 0;//체력
-}stat_struct;
-//스테이터스
-typedef struct {
-	int hp;
-	int mp;
-	int atk;
-	int def;
-	int cri;
-}status;
-//선택지 구조체
-typedef struct {
-	string arr[100];
-	int SIZE;
-}selection_array;
-//소지 구조체
-typedef struct {
-	string arr[1000];
-	int idx;
-}inventory;
+enum weapons {
+	//레이피어
+	//에페
+	EPEE,
+	//플뢰레
+	FLEURET,
+	//스몰소드
+	SMALLSWORD,
+	//에스파다 로페라
+	ESPADAROPERA,
+	//미스틸테인
+	MISTILTEINN,
+	//검
+	//롱소드
+	LONGSWORD,
+	//발키리소드
+	VALKYRIESWORD,
+	//글라디우스
+	GLADIUS,
+	//바스타드소드
+	BASTARDSWORD,
+	//후츠노미타마
+	FUTSUNOMITAMA,
+	//쌍룡검
+	DOUBLEDRAGONSWORD,
+	//엑스칼리버
+	EXCALIBUR,
+	//다인슬레이프
+	DAINSLEIF,
+	//클레이모어
+	CLAYMORE,
+	//프람베르그
+	FLAMBERGE,
+	//마체테
+	MACHETE,
+	//시미터
+	SCIMITAR,
+	//쿠크리
+	KUKRI,
+	//팔시온, 펄션
+	FALCHION,
+	//마법검
+	//티르빙
+	TYRFINGR,
+	//단검
+	//대거
+	DAGGER,
+	//메인고쉬
+	MAINGAUCHE,
+	//클라우솔라스
+	CLAIOMHSOLAIS,
+	//소드브레이커
+	SWORDBREAKER,
+	//창
+	//궁니르
+	GUNGNIR,
+	//롱기누스의창
+	LONGINUS,
+	//루인
+	RUIN,
+	//트리아이나(포세이돈의 창)
+	TRIAINA,
+	//트리슈라(인도의 파괴의 신 시바의 창)
+	TRIDENT,
+	//대검
+	//드래곤슬레이어
+	DRAGONSLAYER,
+	//듀랜달, 뒤랑달
+	DURANDAL,
+	//그람
+	GRAM,
+	//레바테인
+	LAEATEINN,
+	//발뭉
+	BALMUNG,
+	//아론다이트
+	ARONDIGHT,
+	//에케작스
+	ECKESACHS,
+	//망치
+	//묠니르
+	MOLNIR,
+	//대장장이의 망치
+	BLACKSMITHHAMMER
+};
+weapon_status weapons_status[] = {
+	{
+		"에페",UNCOMMON,0,{}
+
+}
+}
 //랜덤확률   0<per<1
 bool random(double per) {
 	if (per <= 0)
